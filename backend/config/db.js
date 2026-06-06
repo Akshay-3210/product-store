25c8755ead05597074ef04871671e2c21f44c9a5
Akshay-3210 <akshaysharma8630292910@gmail.com>
2026-06-06 22:52:52 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD }=process.env;
-// # postgresql://neondb_owner:npg_mnOb6V1IqyLQ@ep-wispy-hall-aqvpqnui-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
-// console.log(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
 export const sql=neon(
     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 )
